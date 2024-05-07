272d4500204b60a0a9377142d759868f0a7f7e9f
danielpsrb <danielpsrb01@gmail.com>
2024-05-07 12:54:21 +0700

    first commit

---
+++ index.ts
@@ -0,0 +1,11 @@
+import figlet from "figlet";
+
+const server = Bun.serve({
+    port: 3000,
+    fetch(req) {
+        const body = figlet.textSync("Hello I am testing Bun on the Windows!");
+        return new Response(body);
+    },
+});
+
+console.log(`Listening on http://localhost:${server.port} ...`);
